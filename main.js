b4599c23bd0213cfc45ec9efe647227cf4910cb9
Bcez <80232341+Bcez@users.noreply.github.com>
2022-03-22 17:25:55 -0700

    SDF
     adsSDF

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("oi mate");
+var firstname = "Ranbir";
+var lastname = "kapoor";
+var fullname = firstname.concat(lastname);
+console.log(fullname); 
+
+var x = 15;
+var y = 5;
+console.log(x+y);
+console.log(x/y);
+console.log(x-y);
+console.log(x*y);
